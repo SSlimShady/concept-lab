cac941d68adf43efce1e7b714b200b33e73e8b04
Sushant Parashar <sushantparashar2@gmail.com>
2025-07-25 10:30:57 +0530

    removed sample boilerplate code

--- apps/frontend/app/api/sql/joins/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(req: NextRequest) {
-  const res = await fetch("http://localhost:8000/api/sql/joins");
-  const data = await res.json();
-  return NextResponse.json(data);
-}
